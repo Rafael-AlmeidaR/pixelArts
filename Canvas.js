258756b714e5b597c159cb40058f7b502c94597c
Rafael-AlmeidaR <rafael.almeida22@yahoo.com>
2025-07-24 20:52:31 -0300

    pipeta e melhora no sistema de import

--- Canvas.js
+++ Canvas.js
@@ -21,12 +21,12 @@ class Grid
 class PixelArt
 {   constructor()
     {   
-        this.width = Number(prompt("Largura?"));
-        this.height = Number(prompt("Altura?"));
-        // this.width = 32;
-        // this.height = 32;
+        // this.width = Number(prompt("Largura?"));
+        // this.height = Number(prompt("Altura?"));
+        this.width = 32;
+        this.height = 32;
         this.art = [];
-        this.drawColor = "rgba(0, 0, 0)";
+        this.drawColor = "#000000";
 
         this.gridWidth = canvas.width/this.width;
         this.gridHeight = canvas.height/this.height;
@@ -58,7 +58,7 @@ class PixelArt
         
 class Tools
 {   constructor()
-    {   this.allTools = [new Pencil(), new Rubber(), new Bucket()];
+    {   this.allTools = [new Pencil(), new Rubber(), new Bucket(), new ColorPicker()];
         this.tool = this.allTools[0];
     }
     switchTool(newTool)
@@ -70,7 +70,7 @@ class Tool
     {   this.points = [];
     }
     clicked()
-    {   console.log("em desenvolvimento!")
+    {   if (pixelArt.art[this.points[0].y]?.[this.points[0].x]?.color === undefined) return;
     }
 }
 // class Pencil extends Tool
@@ -86,7 +86,8 @@ class Pencil extends Tool
     {   super();
     }
     clicked()
-    {   let dx = this.points[1].x - this.points[0].x
+    {   super.clicked();
+        let dx = this.points[1].x - this.points[0].x
         let dy = this.points[1].y - this.points[0].y
         let testePoints = [{x: this.points[0].x, y: this.points[0].y}, {x: this.points[1].x, y: this.points[1].y}];
         //console.log(testePoints)
@@ -137,7 +138,8 @@ class Rubber extends Tool
     {   super();
     }
     clicked()
-    {   let dx = this.points[1].x - this.points[0].x
+    {   super.clicked();
+        let dx = this.points[1].x - this.points[0].x
         let dy = this.points[1].y - this.points[0].y
         let testePoints = [{x: this.points[0].x, y: this.points[0].y}, {x: this.points[1].x, y: this.points[1].y}];
         //console.log(testePoints)
@@ -186,7 +188,7 @@ class Bucket extends Tool
     {   super();
     }
     clicked()
-    {   if (pixelArt.art[this.points[0].y][this.points[0].x]?.color === undefined) return;
+    {   super.clicked();
         let initalColor = pixelArt.art[this.points[0].y][this.points[0].x].color;
         let switchColors = [[this.points[0].x, this.points[0].y]];
         let visitedPositions = [];
@@ -208,70 +210,16 @@ class Bucket extends Tool
             switchColors.shift();
             //console.log(switchColors);
         }
-        // if(pixelArt.art[x][y].color == initalColor)
-        // {   pixelArt.art[x][y].color = pixelArt.drawColor;
-        // }
     }
 }
 
 
-// teste
-// class Bucket extends Tool
-// {   constructor()
-//     {   super();
-//         this.switchColors;
-//         this.visitedPositions;
-//         this.initalColor;
-//         this.needUpdate = false;
-//     }
-//     clicked(x, y, cell)
-//     {   this.initalColor = cell.color;
-//         this.switchColors = [[x, y]];
-//         this.visitedPositions = [];
-        
-//         let xA = this.switchColors[0][0];
-//         let yA = this.switchColors[0][1];
-//         if(!this.visitedPositions.includes(`${xA},${yA}`))
-//         {   let row = pixelArt?.art?.[xA];
-//             let pixel = row?.[yA];
-//             if(pixel)
-//             {   if(pixelArt.art[xA][yA].color == this.initalColor)
-//                 {   pixelArt.art[xA][yA].color = pixelArt.drawColor;
-//                     this.switchColors.push([xA, yA-1], [xA-1, yA], [xA+1, yA], [xA, yA+1]);
-//                 }
-//             }
-//             this.visitedPositions.push(`${xA},${yA}`)
-//         }
-//         this.switchColors.shift();
-            
-//             // console.log(switchColors);
-        
-//         if(pixelArt.art[x][y].color == this.initalColor)
-//         {   pixelArt.art[x][y].color = pixelArt.drawColor;
-//         }
-//         this.needUpdate = true;
-//     }
-//     update()
-//     {   if(this.switchColors.length > 0)
-//         {   let xA = this.switchColors[0][0];
-//             let yA = this.switchColors[0][1];
-//             if(!this.visitedPositions.includes(`${xA},${yA}`))
-//             {   let row = pixelArt?.art?.[xA];
-//                 let pixel = row?.[yA];
-//                 if(pixel)
-//                 {   if(pixelArt.art[xA][yA].color == this.initalColor)
-//                     {   pixelArt.art[xA][yA].color = pixelArt.drawColor;
-//                         this.switchColors.push([xA, yA-1], [xA-1, yA], [xA+1, yA], [xA, yA+1]);
-//                     }
-//                 }
-//                 this.visitedPositions.push(`${xA},${yA}`)
-//             }
-//             this.switchColors.shift();
-            
-//             // console.log(switchColors);
-//         }
-//         if(pixelArt.art[x][y].color == this.initalColor)
-//         {   pixelArt.art[x][y].color = pixelArt.drawColor;
-//         }
-//     }
-// }
+class ColorPicker extends Tool
+{   constructor()
+    {   super();
+    }
+    clicked()
+    {   if (pixelArt.art[this.points[1].y]?.[this.points[1].x]?.color === undefined) return;
+        pixelArt.drawColor = pixelArt.art[this.points[1].y][this.points[1].x].color;
+    }
+}
